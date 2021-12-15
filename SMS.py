a4270482daa326610f964775f86858ad99cb004c
Ashu4638 <84519885+Ashu4638@users.noreply.github.com>
2021-12-16 00:15:06 +0530

    Update SMS.py

--- SMS.py
+++ SMS.py
@@ -7,7 +7,7 @@ def send_sms(message,mob_no):
 	url = "https://www.fast2sms.com/dev/bulk"
 	payload = "sender_id=FSTSMS&message="+ message +"&language=english&route=p&numbers="+ mob_no +""
 	headers = {
-	 'authorization': "BqhIwETQDebOato1vnk2RUWdSmjX3H8LrFGu6fNiC97MlAKgx0FNfI2ZEaWHXdPzLpjn730ycirKkBRg",
+	 'authorization': "AUHTORIZATION_KEY FAST2SMS",
 	 'Content-Type': "application/x-www-form-urlencoded",
 	 'Cache-Control': "no-cache",
 	 }
